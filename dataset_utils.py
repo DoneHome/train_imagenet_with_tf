57e9e04725f80f3780c0dfd592158e12bde6fe34
donghao1 <donehomer@gmail.com>
2017-08-01 20:23:54 +0800

    fix convert tfRecord

--- dataset_utils.py
+++ dataset_utils.py
@@ -19,12 +19,8 @@ from __future__ import print_function
 
 import os
 import sys
-import tarfile
 
-from six.moves import urllib
 import tensorflow as tf
-
-LABELS_FILENAME = 'labels.txt'
 
 
 def int64_feature(values):
@@ -62,53 +58,5 @@ def image_to_tfexample(image_data, image_format, height, width, class_id):
       'image/width': int64_feature(width),
   }))
 
-def write_label_file(labels_to_class_names, dataset_dir,
-                     filename=LABELS_FILENAME):
-  """Writes a file with the list of class names.
-
-  Args:
-    labels_to_class_names: A map of (integer) labels to class names.
-    dataset_dir: The directory in which the labels file should be written.
-    filename: The filename where the class names are written.
-  """
-  labels_filename = os.path.join(dataset_dir, filename)
-  with tf.gfile.Open(labels_filename, 'w') as f:
-    for label in labels_to_class_names:
-      class_name = labels_to_class_names[label]
-      f.write('%d:%s\n' % (label, class_name))
 
 
-def has_labels(dataset_dir, filename=LABELS_FILENAME):
-  """Specifies whether or not the dataset directory contains a label map file.
-
-  Args:
-    dataset_dir: The directory in which the labels file is found.
-    filename: The filename where the class names are written.
-
-  Returns:
-    `True` if the labels file exists and `False` otherwise.
-  """
-  return tf.gfile.Exists(os.path.join(dataset_dir, filename))
-
-
-def read_label_file(dataset_dir, filename=LABELS_FILENAME):
-  """Reads the labels file and returns a mapping from ID to class name.
-
-  Args:
-    dataset_dir: The directory in which the labels file is found.
-    filename: The filename where the class names are written.
-
-  Returns:
-    A map from a label (integer) to class name.
-  """
-  labels_filename = os.path.join(dataset_dir, filename)
-  with tf.gfile.Open(labels_filename, 'r') as f:
-    lines = f.read().decode()
-  lines = lines.split('\n')
-  lines = filter(None, lines)
-
-  labels_to_class_names = {}
-  for line in lines:
-    index = line.index(':')
-    labels_to_class_names[int(line[:index])] = line[index+1:]
-  return labels_to_class_names
--- train.sh
+++ train.sh
@@ -9,10 +9,14 @@
 
 
 # Download the dataset
+CUDA_VISIBLE_DEVICES=0 python convert_data.py \
+  --dataset_dir='/data/donghao1/project/imagenet/images' \
+  --train_label_dir='/data/donghao1/project/imagenet/tool/train.txt' \
+  --val_label_dir='/data/donghao1/project/imagenet/tool/val.txt'
 
 # Train model
-CUDA_VISIBLE_DEVICES=0 python train_image_classifier.py --conf=conf/train.conf
+#CUDA_VISIBLE_DEVICES=0 python train_image_classifier.py --conf=conf/train.conf
 
 # Evaluate model
-CUDA_VISIBLE_DEVICES=0 python eval_image_classifier.py --conf=conf/eval.conf
+#CUDA_VISIBLE_DEVICES=0 python eval_image_classifier.py --conf=conf/eval.conf
 
