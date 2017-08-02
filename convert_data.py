77272e14435d8b3d28ebf76321ded97e320fdd5d
donghao1 <donehomer@gmail.com>
2017-08-02 14:44:04 +0800

    fix

--- convert_data.py
+++ convert_data.py
@@ -53,15 +53,14 @@ class ImageReader(object):
 
     def __init__(self):
         # Initializes function that decodes RGB JPEG data.
-        # decode_image is a Convenience function for decode_gif, decode_jpeg, and decode_png.
         self._decode_jpeg_data = tf.placeholder(dtype=tf.string)
-        self._decode_jpeg = tf.image.decode_image(self._decode_jpeg_data, channels=3)
+        self._decode_jpeg = tf.image.decode_jpeg(self._decode_jpeg_data, channels=3)
 
     def read_image_dims(self, sess, image_data):
-        image = self.decode_image(sess, image_data)
+        image = self.decode_jpeg(sess, image_data)
         return image.shape[0], image.shape[1]
 
-    def decode_image(self, sess, image_data):
+    def decode_jpeg(self, sess, image_data):
         image = sess.run(self._decode_jpeg,
                         feed_dict={self._decode_jpeg_data: image_data})
         assert len(image.shape) == 3
