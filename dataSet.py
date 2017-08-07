c1f9f12c2e25997ab5a7d7523300f72ea3d9a251
donghao1 <donehomer@gmail.com>
2017-08-07 15:31:44 +0800

    add summary

--- dataSet.py
+++ dataSet.py
@@ -54,67 +54,69 @@ def read_my_file_format(file_queue):
 def preprocess_image(image):
     """
     """
-    # Resize Image
-    # TODO: Consider the origin aspect ratio
-    image.set_shape([None, None, 3])
-    distorted_image = tf.image.resize_images(image, [256, 256])
-    # Randomly crop a [height, width] section of the image.
-    distorted_image = tf.random_crop(distorted_image, [IMAGE_SIZE, IMAGE_SIZE,3])
-    # Randomly flip the image horizontally.
-    distorted_image = tf.image.random_flip_left_right(distorted_image)
-    # Randomly distort color
-    #distorted_image = tf.image.random_brightness(distorted_image, max_delta=0.2)
-    #distorted_image = tf.image.random_contrast(distorted_image, lower=0.5, upper=1.5)
-    # Subtract off the mean and divide by the variance of the pixels
-    #distorted_image = tf.image.per_image_standardization(distorted_image)
+    with tf.name_scope('distort_image'):
+        # Resize Image
+        # TODO: Consider the origin aspect ratio
+        image.set_shape([None, None, 3])
+        distorted_image = tf.image.resize_images(image, [256, 256])
+        # Randomly crop a [height, width] section of the image.
+        distorted_image = tf.random_crop(distorted_image, [IMAGE_SIZE, IMAGE_SIZE,3])
+        # Randomly flip the image horizontally.
+        distorted_image = tf.image.random_flip_left_right(distorted_image)
+        # Randomly distort color
+        #distorted_image = tf.image.random_brightness(distorted_image, max_delta=0.2)
+        #distorted_image = tf.image.random_contrast(distorted_image, lower=0.5, upper=1.5)
+        # Subtract off the mean and divide by the variance of the pixels
+        #distorted_image = tf.image.per_image_standardization(distorted_image)
 
-    #distorted_image = tf.clip_by_value(distorted_image, 0.0, 1.0)
+        #distorted_image = tf.clip_by_value(distorted_image, 0.0, 1.0)
 
     return distorted_image
 
 def distort_input(dataset_dir, batch_size, num_reader, num_preprocess_thread):
     """
     """
+    with tf.name_scope('batch_processing'):
+        # TODO: use tf.train.match_filenames_once
+        file_names = get_files_name(dataset_dir)
 
-    # TODO: use tf.train.match_filenames_once
-    file_names = get_files_name(dataset_dir)
+        # create a queue that produces the filenames to read
+        file_queue = tf.train.string_input_producer(file_names)
 
-    # create a queue that produces the filenames to read
-    file_queue = tf.train.string_input_producer(file_names)
+        # create a queue that stores the files that read by readers
+        example_queue = tf.RandomShuffleQueue(
+                capacity = 20 * batch_size,
+                min_after_dequeue = 3 * batch_size,
+                dtypes = [tf.string]
+                )
 
-    # create a queue that stores the files that read by readers
-    example_queue = tf.RandomShuffleQueue(
-            capacity = 20 * batch_size,
-            min_after_dequeue = 3 * batch_size,
-            dtypes = [tf.string]
-            )
+        # create multiple readers to populate the Example Queue
+        enqueue_ops = []
 
-    # create multiple readers to populate the Example Queue
-    enqueue_ops = []
+        for i in range(num_reader):
+            serialized_example = read_my_file_format(file_queue)
+            enqueue_ops.append(example_queue.enqueue([serialized_example]))
 
-    for i in range(num_reader):
-        serialized_example = read_my_file_format(file_queue)
-        enqueue_ops.append(example_queue.enqueue([serialized_example]))
+        tf.train.queue_runner.add_queue_runner(
+            tf.train.queue_runner.QueueRunner(example_queue, enqueue_ops))
 
-    tf.train.queue_runner.add_queue_runner(
-        tf.train.queue_runner.QueueRunner(example_queue, enqueue_ops))
+        serialized_example = example_queue.dequeue()
 
-    serialized_example = example_queue.dequeue()
+        # create multiple threads to preprocess the image
+        example_list = []
+        for i in range(num_preprocess_thread):
+            image, label = parse_proto(serialized_example)
+            image = preprocess_image(image)
+            example_list.append([image, label])
 
-    # create multiple threads to preprocess the image
-    example_list = []
-    for i in range(num_preprocess_thread):
-        image, label = parse_proto(serialized_example)
-        image = preprocess_image(image)
-        example_list.append([image, label])
+        image_batch, label_batch = tf.train.batch_join(
+                tensors_list = example_list,
+                batch_size = batch_size,
+                capacity = 2 * num_preprocess_thread * batch_size
+                )
 
-    image_batch, label_batch = tf.train.batch_join(
-            tensors_list = example_list,
-            batch_size = batch_size,
-            capacity = 2 * num_preprocess_thread * batch_size
-            )
-
-    image_batch = tf.image.convert_image_dtype(image_batch, dtype=tf.uint8)
+        image_batch = tf.image.convert_image_dtype(image_batch, dtype=tf.uint8)
+        tf.summary.image('images', image_batch)
 
     return image_batch, label_batch
 
--- train_image_classifier.py
+++ train_image_classifier.py
@@ -19,14 +19,20 @@ FLAGS = tf.app.flags.FLAGS
 tf.app.flags.DEFINE_string('dataset_dir', None, 'The directory where the dataset files are stored.')
 tf.app.flags.DEFINE_integer("start_gpu_device", 0, 'The start gpu device index')
 tf.app.flags.DEFINE_integer('eval_gpu_device', 0, 'The gpu device used for eval')
-tf.app.flags.DEFINE_integer('num_gpus', 4, 'The number of gpus used for training')
+tf.app.flags.DEFINE_integer('num_gpus', 1, 'The number of gpus used for training')
 
 tf.app.flags.DEFINE_integer('num_reader', '4', 'The number of parallel readers that read data from the dataset.')
 tf.app.flags.DEFINE_integer('num_preprocess_thread', '4', 'The number of threads that preprocess the image.')
 
+tf.app.flags.DEFINE_string('optimizer', 'adam', 'The name of the optimizer, one of "adadelta", "adagrad", "adam", "ftrl", "momentum", "sgd" or "rmsprop"')
+
 tf.app.flags.DEFINE_integer('batch_size', 50, 'The number of batch to train in one iteration')
 tf.app.flags.DEFINE_integer('num_epochs', 10, 'The number of epochs to run')
 tf.app.flags.DEFINE_float('learning_rate', 0.001, 'Initial learning rate')
+tf.app.flags.DEFINE_float('weight_decay', 0.00004, 'The weight decay on the model weights')
+
+tf.app.flags.DEFINE_float('adam_beta1', 0.9, 'The exponential decay rate for the 1st moment estimates.')
+tf.app.flags.DEFINE_float('adam_beta2', 0.999, 'The exponential decay rate for the 2nd moment estimates.')
 
 
 def main(argv=None):
@@ -35,8 +41,10 @@ def main(argv=None):
         raise ValueError('You must supply the dataset directory with --dataset_dir')
 
     with tf.Graph().as_default():
+        #Force all input processing onto CPU in order to reserve the GPU for the forward inference and back-propagation.
         with tf.device('/cpu:0'):
             image_batch, label_batch = DataProvider.distort_input(FLAGS.dataset_dir, FLAGS.batch_size, FLAGS.num_reader, FLAGS.num_preprocess_thread)
+
 
         with tf.Session() as sess:
             init_op = tf.global_variables_initializer()
