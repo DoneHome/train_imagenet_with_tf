fc1c11619c862cc895a143dc881bfc0e8071b1f6
donghao1 <donehomer@gmail.com>
2017-08-07 16:07:53 +0800

    add import

--- alexnet.py
+++ alexnet.py
@@ -1,5 +1,14 @@
 # coding: utf-8
 
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import math
+import os
+import random
+import sys
+
 import tensorflow as tf
 
 
