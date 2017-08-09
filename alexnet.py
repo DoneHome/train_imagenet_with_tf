103cbb9fbaf1c42957fe1228d96b3577a6e28057
donghao1 <donehomer@gmail.com>
2017-08-09 15:33:33 +0800

    fix bias init

--- alexnet.py
+++ alexnet.py
@@ -52,7 +52,7 @@ class AlexNet(object):
 
             with tf.name_scope('conv2') as scope:
                 w2 = weight_init([5, 5, 96, 256], 0.01, weight_decay, 'w_conv2')
-                b2 = bias_init(0.0, [256], 'b_conv2')
+                b2 = bias_init(1.0, [256], 'b_conv2')
                 conv2 = tf.add(conv2d(pool1, w2, stride=[1, 1], padding='SAME'), b2)
                 relu2 = tf.nn.relu(conv2)
                 norm2 = lrn(relu2, radius=2, alpha=1e-4, beta=0.75, bias=1.0)
@@ -66,13 +66,13 @@ class AlexNet(object):
 
             with tf.name_scope('conv4') as scope:
                 w4 = weight_init([3, 3, 384, 384], 0.01, weight_decay, 'w_conv4')
-                b4 = bias_init(0.0, [384], 'b_conv4')
+                b4 = bias_init(1.0, [384], 'b_conv4')
                 conv4 = tf.add(conv2d(relu3, w4, stride=[1, 1], padding='SAME'), b4)
                 relu4 = tf.nn.relu(conv4)
 
             with tf.name_scope('conv5') as scope:
                 w5 = weight_init([3, 3, 384, 256], 0.01, weight_decay, 'w_conv4')
-                b5 = bias_init(0.0, [256], 'b_conv5')
+                b5 = bias_init(1.0, [256], 'b_conv5')
                 conv5 = tf.add(conv2d(relu4, w5, stride=[1, 1], padding='SAME'), b5)
                 relu5 = tf.nn.relu(conv5)
                 pool5 = max_pool(relu5, size=[3,3], stride=[2,2], padding='VALID')
@@ -84,21 +84,21 @@ class AlexNet(object):
                 flatten_input = tf.reshape(pool5, [-1, flat_dim])
 
                 w_fc6 = weight_init(shape=[flat_dim, 4096], stddev=0.01, weight_decay, name='w_fc6')
-                b_fc6 = bias_init(0.0, [4096], 'b_fc6')
+                b_fc6 = bias_init(1.0, [4096], 'b_fc6')
                 fc6 = tf.add(tf.matmul(flatten_input, w_fc6), b_fc6)
                 relu_fc6 = tf.nn.relu(fc6)
                 dropout_fc6 = tf.nn.dropout(relu_fc6, keep_prob)
 
             with tf.name_scope('fc7') as scope:
                 w_fc7 = weight_init(shape=[4096,  4096], stddev=0.01, weight_decay, name='w_fc7')
-                b_fc7 = bias_init(0.0, [4096], 'b_fc7')
+                b_fc7 = bias_init(1.0, [4096], 'b_fc7')
                 fc7 = tf.add(tf.matmul(dropout_fc6, w_fc7), b_fc7)
                 relu_fc7 = tf.nn.relu(fc7)
                 dropout_fc7 = tf.nn.dropout(relu_fc7, keep_prob)
 
             with tf.name_scope('softmax') as scope:
                 w_softmax = weight_init(shape=[4096,  1000], stddev=0.01, weight_decay, name='w_softmax')
-                b_softmax = bias_init(0.0, [1000], 'b_softmax')
+                b_softmax = bias_init(1.0, [1000], 'b_softmax')
                 fc8 = tf.add(tf.matmul(dropout_fc7, w_softmax), b_softmax)
                 output = tf.nn.softmax(fc8)
 
